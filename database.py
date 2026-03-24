6c9cb3da6f6bf3eb1ff968518f48adbf1bfbfbea
adrienz1 <djiengou@ualberta.ca>
2026-03-24 16:04:21 -0600

    added some basic html for the app's pages. Srted the implementation of the database access

--- database.py
+++ database.py
@@ -2,18 +2,46 @@ import os
 import json
 from pymongo import MongoClient
 
-#  Insert the family data from a json file into the MongoDB database. The JSON file is created by the parse.py script and contains the family data extracted from the text file.
-def upload_data():
-    url = os.getenv("MONGO_URI")
-    client = MongoClient(url)
-    db = client["family"]
-    collection = db["family_members"]
+class Database:
+    def __init__(self):
+        url = os.getenv("MONGO_URI")
+        self.client = MongoClient(url)
+        self.db = self.client["family"]
+        self.collection = self.db["family_members"]
     
-    with open("family.json", "r", encoding="utf-8") as f:
-        family_data = json.load(f)
+    #  Insert the family data from a json file into the MongoDB database. The JSON file is created by the parse.py script and contains the family data extracted from the text file.
+    def upload_data(self):
+        with open("family.json", "r", encoding="utf-8") as f:
+            family_data = json.load(f)
+            
+        for person in family_data:
+            person["_id"] = person.pop("id")
+            
+        self.collection.insert_many(family_data)
+        print("Inserted family data into MongoDB")
         
-    for person in family_data:
-        person["_id"] = person.pop("id")
+    def find_person_by_id(self, person_id):
+        person = self.collection.find_one({"_id": person_id})
+        client = MongoClient(url)
+        db = client["family"]
+        collection = db["family_members"]
         
-    collection.insert_many(family_data)
-    print("Inserted family data into MongoDB")
+        person = collection.find_one({"_id": person_id})
+        return person
+
+    def find_parents(self, person_id):
+        person = self.collection.find_one({"_id": person_id})
+        if not person:
+            return None
+        
+        parents = []
+        for parent_id in person.get("parents", []):
+            parent = self.collection.find_one({"_id": parent_id})
+            if parent:
+                parents.append(parent)
+        
+        return parents
+    
+    def find_children(self, person_id):
+        children = self.collection.find({"parents": person_id})
+        return list(children)   
